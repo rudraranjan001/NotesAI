63d2f4e064c7fa7affaa4819e042846bfdbd1adc
Rudra <rudraranjan2003@gmail.com>
2026-06-12 18:49:38 +0530

    changes in api for tokens

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,18 @@
 import axios from "axios";
+import { auth } from "../config/firebase";
 
 const api = axios.create({
     baseURL : "http://localhost:8000/api",
 });
+
+api.interceptors.request.use(async(config) => {
+    const user = auth.currentUser;
+
+    if(user){
+        const token = await user.getIdToken();
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+})
 
 export default api;
