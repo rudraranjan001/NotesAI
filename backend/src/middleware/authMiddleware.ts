46177386f148c87a0709b4ec51a5d368fef82505
Rudra <rudraranjan2003@gmail.com>
2026-06-13 18:31:10 +0530

    added middleware

---
+++ backend/src/middleware/authMiddleware.ts
@@ -0,0 +1,28 @@
+import type{ Request , Response, NextFunction } from "express";
+
+import { adminAuth } from "../config/firebaseAdmin";
+
+export const authMiddleware = async(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader){
+        return res.status(401).json({message: "No token provided"});
+    }
+    if(!authHeader.startsWith("Bearer ")){
+        return res.status   (401).json({message: "Inavlid token format"});
+    }
+    const token = authHeader.split(" ")[1];
+    try {
+  const decodedToken = await adminAuth.verifyIdToken(token);
+
+  (req as any).uid = decodedToken.uid;
+
+  next();
+} catch (error) {
+  return res.status(401).json({ message: "Invalid or expired token" });
+}
+};
