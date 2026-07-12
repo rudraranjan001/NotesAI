74f26cd5b3d28fbee1a80fbbb2acb89926714087
Rudra <rudraranjan2003@gmail.com>
2026-07-12 22:29:47 +0530

    fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
   useEffect(() => {
     const timerId = window.setTimeout(() => {
       setIntroVisible(false);
-    }, 2000);
+    }, 2000 );
 
     return () => window.clearTimeout(timerId);
   }, []);
